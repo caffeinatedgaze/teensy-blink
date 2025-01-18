23941d2ae6c24a49a293663a44438c52df2d2013
Mikhail Liamets <m.liamets@jacobs-university.de>
2025-01-18 23:31:48 +0100

    pattern-executor: sketch

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,39 @@
 #include "morse.hpp"
 #include "main.hpp"
 
-SoftwareSerial printer(21, 20);
+enum class PatternType
+{
+	ZigZag,
+	Random
+};
+
+enum class TeensyType
+{
+	Primary,
+	Secondary
+};
+
+// Changes the state of the laser array according to a pattern: zigzag or random.
+class PatternExecutor
+{
+public:
+	PatternType patternType;
+	TeensyType currentTeensyType = TeensyType::Primary;
+	LaserStates &laserStates;
+
+	PatternExecutor(PatternType patternType, LaserStates &laserStates) : patternType(patternType), laserStates(laserStates)
+	{
+	}
+
+	// Choose the next laser according to the pattern.
+	void chooseNextLaser();
+
+	// Update the laser array in the primary as well as send command to the secondary.
+	void setLaserState(bool laserState);
+};
+
+LaserStates laserStates;
+PatternExecutor *patternExecutor;
 
 void setup()
 {
@@ -15,7 +47,13 @@ void setup()
 	}
 	Serial.println("Serial port is ready.");
 	pinMode(LED_BUILTIN, OUTPUT);
-	printer.begin(9600);
+
+	patternExecutor = new PatternExecutor(PatternType::ZigZag, laserStates);
+}
+
+void teardown()
+{
+	delete patternExecutor;
 }
 
 void loop()
@@ -24,4 +62,13 @@ void loop()
 	delay(1000);
 	analogWrite(LED_BUILTIN, LOW);
 	delay(1000);
+
+	// initialize pattern producer with the matrix and its dimensions
+	// getCodepoint from the translated sequence.
+	// convert codepoint into morse code (i.e. Dits, Dahs, and breaks).
+	// iterate over each signal of the codepoint's morse code and
+	// pass its value - on/off - into the current pattern producer (i.e. ZigZag or Random).
+	//
+	// wait for the break duration.
+	// ... repeat.
 }
