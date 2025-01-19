2d06d0ad5998ea22b5d1665413e41858cfeef178
Mikhail Liamets <m.liamets@jacobs-university.de>
2025-01-19 19:08:31 +0100

    pio: anchor deps

--- Makefile
+++ Makefile
@@ -1,12 +1,16 @@
 .PHONY: test
 test:
-	pio test ${KARGS}
+	pio test -e test ${KARGS}
 
 .PHONY: upload 
 upload:
-	pio run --target upload
+	pio run -e release --target upload
 	pio device monitor
 
 .PHONY: build
 build:
 	pio run
+
+.PHONY: install-lib
+install-lib:
+	pio pkg install --library
--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,15 @@
 #include <cstdlib>
 #include <iostream>
 #include <SoftwareSerial.h>
+#include <Adafruit_AW9523.h>
+
 #include "morse.hpp"
 #include "codepoints.hpp"
 #include "patternExecutor.hpp"
 #include "main.hpp"
 
 PatternExecutor *patternExecutor;
+
 LaserStates laserStates;
 uint64_t currentCodepointIdx = 0;
 std::string currentCodepoint;
@@ -26,6 +29,7 @@ int freeram()
 
 void setup()
 {
+	// while (!bitRead(USB1_PORTSC1, 7) && !Serial && millis() < 15000)
 	while (!Serial && millis() < 15000)
 	{
 		// wait for Arduino Serial Monitor to be ready
@@ -40,6 +44,23 @@ void setup()
 		std::cout << "CODEPOINTS cannot be of zero length. Aborting.";
 		exit(1);
 	}
+
+	// todo: enable all of the pins.
+	for (uint8_t i = 0; i < LASER_ARRAY_X; i++)
+	{
+		for (uint8_t j = 0; j < LASER_ARRAY_Y; j++)
+		{
+			pinMode(j, OUTPUT);
+		}
+	}
+
+	// Adafruit_AW9523 aw;
+	// if (!aw.begin(0x58))
+	// {
+	// 	std::cout << "AW9523 not found? Check wiring!" << std::endl;
+	// 	while (1)
+	// 		delay(10); // halt forever
+	// }
 }
 
 void teardown()
@@ -49,49 +70,54 @@ void teardown()
 
 void loop()
 {
+	for (uint8_t j = 0; j < 100; j++)
+	{
+		digitalWrite(j, LOW);
+	}
+
 	try
 	{
-		std::cout << "Picking the next codepoint." << std::endl;
-		currentCodepoint = CODEPOINTS.at(currentCodepointIdx);
-		currentCodepointMorseCode = encodeNumeral(currentCodepoint.c_str());
-		printSignals(currentCodepointMorseCode);
-		std::cout << "Morse code length: " << currentCodepointMorseCode.size() << std::endl;
+	std::cout << "Picking the next codepoint." << std::endl;
+	currentCodepoint = CODEPOINTS.at(currentCodepointIdx);
+	currentCodepointMorseCode = encodeNumeral(currentCodepoint.c_str());
+	printSignals(currentCodepointMorseCode);
+	std::cout << "Morse code length: " << currentCodepointMorseCode.size() << std::endl;
 
-		for (size_t i = 0; i < currentCodepointMorseCode.size(); i++)
+	for (size_t i = 0; i < currentCodepointMorseCode.size(); i++)
+	{
+		SignalPtr signal = currentCodepointMorseCode.at(i);
+
+		// Set laser state according to the signal.
+		patternExecutor->setLaserState(signal->value);
+		std::cout << "Current signal type: " << signal->getType() << std::endl;
+		printLaserStates(laserStates);
+		// Blink the built-in LED.
+		analogWrite(LED_BUILTIN, signal->value ? HIGH : LOW);
+		// Wait for the signal duration.
+		while (signal->counter < signal->max_value)
 		{
-			SignalPtr signal = currentCodepointMorseCode.at(i);
-
-			// Set laser state according to the signal.
-			patternExecutor->setLaserState(signal->value);
-			std::cout << "Current signal type: " << signal->getType() << std::endl;
-			printLaserStates(laserStates);
-			// Blink the built-in LED.
-			analogWrite(LED_BUILTIN, signal->value ? HIGH : LOW);
-			// Wait for the signal duration.
-			while (signal->counter < signal->max_value)
+			delay(refreshRate);
+			signal->counter++;
+		}
+		// Move to the next laser according to the pattern.
+		// Do not move if the next signal type is a break.
+		if (i + 1 < currentCodepointMorseCode.size())
+		{
+			std::string currentCodepointType = currentCodepointMorseCode.at(i + 1)->getType();
+			if (
+				currentCodepointType != LETTER_BREAK &&
+				currentCodepointType != WORD_BREAK &&
+				currentCodepointType != SENTENCE_BREAK)
 			{
-				delay(refreshRate);
-				signal->counter++;
-			}
-			// Move to the next laser according to the pattern.
-			// Do not move if the next signal type is a break.
-			if (i + 1 < currentCodepointMorseCode.size())
-			{
-				std::string currentCodepointType = currentCodepointMorseCode.at(i + 1)->getType();
-				if (
-					currentCodepointType != LETTER_BREAK &&
-					currentCodepointType != WORD_BREAK &&
-					currentCodepointType != SENTENCE_BREAK)
-				{
-					patternExecutor->chooseNextLaser();
-				}
+				patternExecutor->chooseNextLaser();
 			}
 		}
+	}
 
-		currentCodepointIdx = (currentCodepointIdx + 1) % CODEPOINTS.size();
-		std::cout << "Current codepoint Idx: " << currentCodepointIdx << std::endl;
-		std::cout << "Codepoints length: " << CODEPOINTS.size() << std::endl;
-		std::cout << "Free RAM: " << freeram() << std::endl;
+	currentCodepointIdx = (currentCodepointIdx + 1) % CODEPOINTS.size();
+	std::cout << "Current codepoint Idx: " << currentCodepointIdx << std::endl;
+	std::cout << "Codepoints length: " << CODEPOINTS.size() << std::endl;
+	std::cout << "Free RAM: " << freeram() << std::endl;
 	}
 	catch (const std::exception &e)
 	{
