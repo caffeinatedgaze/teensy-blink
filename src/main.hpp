e6801cb3793f9fa9ad4820fc1bac69897848059a
Mikhail Liamets <m.liamets@jacobs-university.de>
2025-01-19 12:47:34 +0100

    laser-states: add defaults

--- src/main.hpp
+++ src/main.hpp
@@ -3,8 +3,8 @@
 struct LaserStates
 // States (on/off) of lasers in each array.
 {
-	bool primaryLaserStates[24][24]; // = {false};
-	bool secondaryLaserStates[24][24];//  = {false};
+	bool primaryLaserStates[24][24] = {false};
+	bool secondaryLaserStates[24][24] = {false};
 };
 
 enum class TeensyType
